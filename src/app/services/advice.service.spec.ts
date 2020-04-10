7aabb4e105b85924257372e48e1fa6d45eaec043
Богдан <zvyagintsevbi@pik.ru>
2020-04-11 00:20:59 +0300

    tests: update tests

--- src/app/services/advice.service.spec.ts
+++ src/app/services/advice.service.spec.ts
@@ -17,11 +17,17 @@ describe('AdviceService', () => {
   });
 
   it('should return advice', () => {
+    expect(service.getAdvice('wind')).toEqual(advices.wind);
     expect(service.getAdvice('rain')).toEqual(advices.rain);
     expect(service.getAdvice('snow')).toEqual(advices.snow);
-    expect(service.getAdvice('hot')).toEqual(advices.hot);
+    expect(service.getAdvice('rainSnow')).toEqual(advices.rainSnow);
+    expect(service.getAdvice('warm')).toEqual(advices.warm);
+    expect(service.getAdvice('warm10')).toEqual(advices.warm10);
+    expect(service.getAdvice('warm20')).toEqual(advices.warm20);
     expect(service.getAdvice('cold')).toEqual(advices.cold);
-    expect(service.getAdvice('wind')).toEqual(advices.wind);
+    expect(service.getAdvice('cold10')).toEqual(advices.cold10);
+    expect(service.getAdvice('warmer')).toEqual(advices.warmer);
+    expect(service.getAdvice('cooling')).toEqual(advices.cooling);
   });
 
   it('should return null if wrong input', () => {
