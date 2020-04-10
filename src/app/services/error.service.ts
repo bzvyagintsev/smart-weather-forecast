bbd7d1d8b044e85a47ecc5ada5fff511714b4836
Богдан <zvyagintsevbi@pik.ru>
2020-04-10 23:04:04 +0300

    feat: update error messages

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -12,14 +12,14 @@ export class ErrorService {
   public handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
+      console.error('Ошибка:', error.error.message);
 
-      this.openSnackBar('An error occurred:' + error.error.message, null);
+      this.openSnackBar('Ошибка:' + error.error.message, null);
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+        `Код ответа сервера ${error.status}, ` + `ошибка: ${error.error}`
       );
 
       if (error.status === 404) {
@@ -29,14 +29,14 @@ export class ErrorService {
         );
       } else {
         this.openSnackBar(
-          `Backend returned code ${error.status}, ` +
-            `body was: ${error.error}`,
+          `Код ответа сервера ${error.status}, ` +
+            `ошибка: ${error.error}`,
           null
         );
       }
     }
     // return an observable with a user-facing error message
-    return throwError('Something bad happened; please try again later.');
+    return throwError('Не получилось загрузить данные, попробуйте ещё раз.');
   }
 
   private openSnackBar(message: string, action: string): void {
